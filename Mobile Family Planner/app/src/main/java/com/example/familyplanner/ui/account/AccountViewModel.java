19f9d20b00ee635b1043ab51cdb2bf7d3f496880
Evan Ward <44804707+EvanWard29@users.noreply.github.com>
2020-11-02 16:06:46 +0000

    Started Interface Implementation
    Started implementing the interface of the application, starting with the "My Family" page.

---
+++ Mobile Family Planner/app/src/main/java/com/example/familyplanner/ui/account/AccountViewModel.java
@@ -0,0 +1,11 @@
+package com.example.familyplanner.ui.account;
+
+import android.text.style.BackgroundColorSpan;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class AccountViewModel extends ViewModel {
+    
+}
